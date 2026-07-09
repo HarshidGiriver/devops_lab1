75391d3908109adf0f3bbd877b567dc14ab409c5
HarshidGiriver <harshidgiriver@gmail.com>
2026-07-09 15:07:22 +0530

    second file added

---
+++ giriver.py
@@ -0,0 +1,14 @@
+# Electricity Bill Calculator
+
+units = int(input("Enter number of units consumed: "))
+
+if units <= 100:
+    bill = units * 2
+elif units <= 200:
+    bill = (100 * 2) + ((units - 100) * 3)
+else:
+    bill = (100 * 2) + (100 * 3) + ((units - 200) * 5)
+
+print("Total Electricity Bill = ₹", bill)
+
+#Second version
